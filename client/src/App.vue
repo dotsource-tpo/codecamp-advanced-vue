7c157b85f303dc4646b6f1808594538d66fed9b1
dotsoure-tpo <t.potte@dotsource.de>
2021-10-26 11:52:49 +0200

    vue basics

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,13 +1,18 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
+    <img src="./assets/logo.png" v-bind:title="imageAlt">
     <router-view/>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'App'
+  name: 'App',
+  data () {
+    return {
+      imageAlt: 'Vue Logo'
+    }
+  }
 }
 </script>
 
